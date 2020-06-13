ca18059658dc2a388b718ef5c19bdfb026212c5a
MH <mh@MH-MacBook-Pro.local>
2020-06-14 01:35:00 +0800

    fix

--- assets/script/platform/PlatformBase.ts
+++ assets/script/platform/PlatformBase.ts
@@ -3,14 +3,24 @@
  */
 export abstract class PlatformBase {
 
+    private _orientation: number = 0;
     /** 屏幕旋转类型 1(竖屏) 2(横屏) */
     public get orientation() {
-        return cc.winSize.width < cc.winSize.height ? 1 : 2;
+        if (!this._orientation) {
+            this._orientation = cc.winSize.width < cc.winSize.height ? 1 : 2;
+        }
+        return this._orientation;
     };
+
+    private _screentype: number = 0;
     /** 屏幕类型 1(16:9) 2(>16:9的全面屏) */
     public get screentype() {
-        return Math.max(cc.winSize.width, cc.winSize.height) / Math.min(cc.winSize.width, cc.winSize.height) > 1.78 ? 2 : 1;;
+        if (!this._screentype) {
+            this._screentype = Math.max(cc.winSize.width, cc.winSize.height) / Math.min(cc.winSize.width, cc.winSize.height) > 1.78 ? 2 : 1;
+        }
+        return this._screentype;
     }
+    
     /** 枚举广告id,在子类中补充对应平台的广告id */
     abstract adUnitId = {
         V_SIGN: "",
